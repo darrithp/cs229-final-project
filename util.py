39da30afb8660c6ce6b10182c899ff752c8f6dd1
Zahra Abdullah <zahraab@stanford.edu>
2018-11-17 15:21:30 -0800

    load_data returns dicts of imgs and genres

--- util.py
+++ util.py
@@ -4,6 +4,8 @@ import pandas as pd
 DATA_PATH = "./movie_data.csv"
 
 def load_data():
-	df = pd.read_csv(DATA_PATH, encoding='latin1', sep=',', header=None)
-	print(df)
-	return df.values
+	df = (pd.read_csv(DATA_PATH, encoding='latin1', sep=',', header=None)).values
+	# 0='imdbId', 1='Imdb Link', 2='Title', 3='IMDB Score', 4='Genre', 5='Poster'
+	imgs = {img_id: img_link for img_id, img_link in df[1:, [0, 5]] if type(img_link) is np.str}
+	genres = {img_id: genre_list.split('|') for img_id, genre_list in df[1:, [0, 4]] if type(genre_list) is np.str and img_id in imgs}
+	return imgs, genres
